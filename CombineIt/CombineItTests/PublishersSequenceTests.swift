45d4e026b2dcd8e6c4d3b56521701619c6eac54b
barbara <barbara@komoot.de>
2022-07-05 21:35:49 +0200

    Adding more examples of Sequence publishers

--- CombineIt/CombineItTests/PublishersSequenceTests.swift
+++ CombineIt/CombineItTests/PublishersSequenceTests.swift
@@ -113,7 +113,7 @@ class PublishersSequenceTests: XCTestCase {
                 satisfy = result
             }
 
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         XCTAssertFalse(satisfy)
@@ -149,7 +149,7 @@ class PublishersSequenceTests: XCTestCase {
                 values.append(number)
             }
 
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         XCTAssertEqual(values.count, 8)
@@ -187,8 +187,7 @@ class PublishersSequenceTests: XCTestCase {
                 values = sequenceIn
             }
 
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         XCTAssertEqual(values.count, 4)
@@ -225,8 +224,7 @@ class PublishersSequenceTests: XCTestCase {
             }
 
 
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         XCTAssertEqual(values.count, 5)
@@ -260,9 +258,7 @@ class PublishersSequenceTests: XCTestCase {
                 count = value
             }
 
-
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         XCTAssertEqual(count, 7)
@@ -302,9 +298,7 @@ class PublishersSequenceTests: XCTestCase {
                 values.append(value)
             }
 
-
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         XCTAssertEqual(values.count, 1)
@@ -341,9 +335,7 @@ class PublishersSequenceTests: XCTestCase {
                 values.append(value)
             }
 
-
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         XCTAssertEqual(values.count, 6)
@@ -378,9 +370,7 @@ class PublishersSequenceTests: XCTestCase {
                 values.append(value)
             }
 
-
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         XCTAssertEqual(values.count, 1)
@@ -416,9 +406,7 @@ class PublishersSequenceTests: XCTestCase {
                 values.append(value)
             }
 
-
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         // the value closure should never be called
@@ -457,8 +445,7 @@ class PublishersSequenceTests: XCTestCase {
             }
 
 
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         // only the first value should be received
@@ -496,9 +483,7 @@ class PublishersSequenceTests: XCTestCase {
                 values.append(value)
             }
 
-
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         // only the first value should be received
@@ -536,8 +521,7 @@ class PublishersSequenceTests: XCTestCase {
             }
 
 
-
-        // wait for the publisher to be processed by allSatisfy instance method
+        // wait for the publisher to be processed
         wait(for: [expectation], timeout: 1.0)
         XCTAssertNotNil(subscription)
         // only the first value should be received
@@ -545,7 +529,330 @@ class PublishersSequenceTests: XCTestCase {
         
     }
     
+    /// Max By: allows to pick the maximum value from a sequence based on an individually defined comparission
+    /// For example when you have a complex struct, and want to determine some sort of sorting based on 1 property of the struct
+    /// Like for example age.
+    /// https://developer.apple.com/documentation/combine/publishers/sequence/max(by:)-1eehr
+    /// this related to the simpler version MAX: https://developer.apple.com/documentation/combine/publishers/sequence/max()-3ikh2
+    /// to MIN: https://developer.apple.com/documentation/combine/publishers/sequence/min()-9fdvw
+    /// and MIN BY: https://developer.apple.com/documentation/combine/publishers/sequence/min(by:)-oi91
+    func testSequenceMaxBy() {
+        
+        // expect for the values to be received
+        let expectation = XCTestExpectation(description: "")
+        
+        struct Example {
+            let id: String
+            let rounds: Int
+        }
+        
+        // the sequence to create a Sequence Publisher
+        let sequence = [Example(id: "1", rounds: 4), Example(id: "3", rounds: 100)]
 
+        // to assign the sequence received
+        var values: [Example] = []
+        
+        // create a sequence publisher and subscribe
+        let subscription = sequence.publisher
+            .max(by: { lhs, rhs in
+                /// this case would be more clear if we would have an struct for e
+                return lhs.rounds > rhs.rounds
+            })
+            .sink { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        XCTFail()
+                }
+                expectation.fulfill()
+            } receiveValue: { value in
+                values.append(value)
+            }
+
+
+        // wait for the publisher to be processed
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(subscription)
+        // only the first value should be received
+        XCTAssertEqual(values.count, 1)
+        
+    }
+    
+    /// Permits to choose 1 element from the sequence publisher, or a range of elements
+    /// If the publisher finishes before the values are received, then the subscriber receives .finished
+    /// https://developer.apple.com/documentation/combine/publishers/sequence/output(at:)-3r7zo
+    func testSequenceOutput() {
+        
+        // expect for the values to be received
+        let expectation = XCTestExpectation(description: "")
+        
+        // the sequence to create a Sequence Publisher
+        let sequence = [1,2,3,4,5,6,7]
+
+        // to assign the sequence received
+        var values: [Int] = []
+        
+        // create a sequence publisher and subscribe
+        let subscription = sequence.publisher
+            .output(in: 5...20)
+            .sink { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        XCTFail()
+                }
+                expectation.fulfill()
+            } receiveValue: { value in
+                values.append(value)
+            }
+
+        // wait for the publisher to be processed
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(subscription)
+        // only 6 and 7 should be received and then the publisher should complete
+        XCTAssertEqual(values.count, 2)
+        
+    }
+    
+    /// Selects the first N elements of a sequence
+    /// https://developer.apple.com/documentation/combine/publishers/sequence/prefix(_:)
+    /// https://developer.apple.com/documentation/combine/publishers/sequence/prefix(while:)
+    func testSequencePrefix() {
+        
+        // expect for the values to be received
+        let expectation = XCTestExpectation(description: "")
+        
+        // the sequence to create a Sequence Publisher
+        let sequence = [1,2,3,4,5,6,7]
+
+        // to assign the sequence received
+        var values: [Int] = []
+        
+        // create a sequence publisher and subscribe
+        let subscription = sequence.publisher
+            .prefix(2)
+            .sink { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        XCTFail()
+                }
+                expectation.fulfill()
+            } receiveValue: { value in
+                values.append(value)
+            }
+
+        // wait for the publisher to be processed
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(subscription)
+        // only the first 2 values should be received
+        XCTAssertEqual(values.count, 2)
+        
+    }
+    
+
+    /// Connect 2 publishers (with same input and output together
+    /// delivering all elements from the first one and then from the second one
+    /// This could be useful when you have 2 APIs call returning a set of date, then you can have a map on both APIs responses
+    /// make the items of the same type, then use prepend to deliver them together
+    /// https://developer.apple.com/documentation/combine/publishers/sequence/prepend(_:)-1r564
+    func testSequencePrepend() {
+        
+        // expect for the values to be received
+        let expectation = XCTestExpectation(description: "")
+        
+        // the sequence to create a Sequence Publisher
+        let sequence1 = [1,2,3,4]
+        let sequence2 = [5,6,7]
+
+        // to assign the sequence received
+        var values: [Int] = []
+        
+        // create a sequence publisher and subscribe
+        let subscription = sequence1.publisher
+            .prepend(sequence2)
+            .sink { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        XCTFail()
+                }
+                expectation.fulfill()
+            } receiveValue: { value in
+                values.append(value)
+            }
+
+        // wait for the publisher to be processed
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(subscription)
+        // only the first 2 values should be received
+        XCTAssertEqual(values.count, 7)
+        
+    }
+    
+    /// Proccesses all the elements and produce 1 output value
+    /// For example: reading all bank transactions of the last week and calculating the total amount of money spent
+    /// https://heckj.github.io/swiftui-notes/#reference-output
+    func testSequenceReduce() {
+        
+        // expect for the values to be received
+        let expectation = XCTestExpectation(description: "")
+        
+        // the sequence to create a Sequence Publisher
+        let sequence = [1,2,3,4]
+
+        // to assign the sequence received
+        var finalValue: Int = 0
+        
+        // create a sequence publisher and subscribe
+        let initialValue = 0
+        let subscription = sequence.publisher
+            .reduce( initialValue, { acc, current in
+                acc + current
+            })
+            .sink { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        XCTFail()
+                }
+                expectation.fulfill()
+            } receiveValue: { value in
+                finalValue = value
+            }
+
+
+
+        // wait for the publisher to be processed
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(subscription)
+        // should return the sum of all elements
+        XCTAssertEqual(finalValue, 10)
+        
+    }
+    
+    /// just removes the duplicated items
+    /// https://developer.apple.com/documentation/combine/publishers/sequence/removeduplicates()-2uq6m
+    func testSequenceRemoveDuplicates() {
+        
+        // expect for the values to be received
+        let expectation = XCTestExpectation(description: "")
+        
+        // the sequence to create a Sequence Publisher
+        let sequence = [1,2,3,4,1,1,1]
+
+        // to assign the sequence received
+        var values: [Int] = []
+        
+        // create a sequence publisher and subscribe
+        let subscription = sequence.publisher
+            .removeDuplicates()
+            .sink { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        XCTFail()
+                }
+                expectation.fulfill()
+            } receiveValue: { value in
+                values.append(value)
+            }
+
+
+
+        // wait for the publisher to be processed
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(subscription)
+        // only 4 elements should persist after the removal of duplictes
+        XCTAssertEqual(values.count, 4)
+        
+    }
+    
+
+    /// Replace the NIL elements with an element of the same type as the initial elements of the sequence
+    /// https://developer.apple.com/documentation/combine/publishers/sequence/replacenil(with:)-4cqz2
+    func testSequenceReplaceNil() {
+        
+        // expect for the values to be received
+        let expectation = XCTestExpectation(description: "")
+        
+        // the sequence to create a Sequence Publisher
+        let sequence = [1,2,3,4,nil]
+
+        // to assign the sequence received
+        var values: [Int?] = []
+        
+        // create a sequence publisher and subscribe
+        let subscription = sequence.publisher
+            .replaceNil(with: 0)
+            .sink { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        XCTFail()
+                }
+                expectation.fulfill()
+            } receiveValue: { value in
+                values.append(value)
+            }
+
+
+
+        // wait for the publisher to be processed
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(subscription)
+        // 5 elements should exist still in the array, after nil is converted to 0
+        XCTAssertEqual(values.count, 5)
+        
+    }
+    
+    func testSequenceScan() {
+        
+        // expect for the values to be received
+        let expectation = XCTestExpectation(description: "")
+        
+        // the sequence to create a Sequence Publisher
+        let sequence = ["H", "E", "I"]
+
+        // to assign the sequence received
+        var values: [String] = []
+        
+        // create a sequence publisher and subscribe
+        let subscription = sequence.publisher
+            .scan("", { acc, current in
+                acc + current
+            })
+            .sink { completion in
+                switch completion {
+                    case .finished:
+                        break
+                    case .failure(_):
+                        XCTFail()
+                }
+                expectation.fulfill()
+            } receiveValue: { value in
+                values.append(value)
+            }
+
+
+
+        // wait for the publisher to be processed
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(subscription)
+        // All intermediate results are pushed by scan, till the sequence is completed
+        XCTAssertEqual(values[0], "H")
+        XCTAssertEqual(values[1], "HE")
+        XCTAssertEqual(values[2], "HEI")
+
+    }
+    
 
 
 }
